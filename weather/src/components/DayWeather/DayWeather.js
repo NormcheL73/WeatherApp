693d9a5846c15c40d4feb2fcc687219c376cfffa
normchel <normchel73@gmail.com>
2022-06-28 21:45:46 +0400

    Проверка на дату(Сегодня-Завтра)

--- weather/src/components/DayWeather/DayWeather.js
+++ weather/src/components/DayWeather/DayWeather.js
@@ -1,20 +1,33 @@
 import React from 'react';
 import './index.css';
 
-function DayWeather({tempDay, tempNight, dt, icon, test}) {
-  console.log(test)
+function DayWeather({tempDay, tempNight, dt, icon}) {
+  console.log(1)
 
-  var myDate = new Date(dt * 1000)
+  let myDate = new Date(dt * 1000)
+  let checkDate = new Date()
+  let toDay = `${myDate.getDate()}.${myDate.getMonth() + 1}.${myDate.getFullYear()}`
+
 
   const correctIcon = `http://openweathermap.org/img/wn/${icon}@2x.png`
-  const toDay = `${myDate.getDate()}.${myDate.getMonth() + 1}.${myDate.getFullYear()}`
+
+
+  if (checkDate.getDate() == myDate.getDate()) {
+    toDay = 'Сегодня'
+  }
+
+  if ((checkDate.getDate() + 1 == myDate.getDate()) || 
+      (checkDate.getDate() == 30 && checkDate.getDate() + 1 == 1 && myDate.getDate() == 1) ||
+      (checkDate.getDate() == 31 && myDate.getDate() == 1)) {
+    toDay = 'Завтра'
+  }
 
   return (
   <div className="DayWeather">
     <span className='DayWeather_title'>{toDay}</span>
     <img src={correctIcon}></img>
     <div className='DayWeather_temp'>
-      <span className='Day_temp'>{Math.round(tempDay)}°   </span>
+      <span className='Day_temp'>{Math.round(tempDay)}°</span>
       <span className='Night_temp'>{Math.round(tempNight)}°</span>
     </div> 
   </div>
